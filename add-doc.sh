7aa7a13c9b2fb8c7dd7b4685ca7ebb6946cc58a1
Joao Gilberto Magalhaes <joao@byjg.com.br>
2021-01-03 12:42:53 -0600

    Add script to update documentation

---
+++ add-doc.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+DOC_FOLDER="$1"
+PROJECT_NAME="$2"
+EXTRA_FOLDER="$3"
+
+if [ -z "$PROJECT_NAME" ]
+then
+  echo "Usage: "
+  echo "  add-doc.sh <doc_folder> <project> [extra_folder]"
+  exit 1
+fi
+
+if [ -z "$DOC_GITHUB_TOKEN" ]
+then
+  echo 'DOC_GITHUB_TOKEN is required'
+  exit 2
+fi
+
+PROJECT_FOLDER="$PWD"
+TMP_FOLDER=/tmp/byjg.github.io
+
+git clone --depth 1 https://byjg:$DOC_GITHUB_TOKEN@github.com/byjg/byjg.github.io $TMP_FOLDER
+
+cp README.md "${TMP_FOLDER}/${DOC_FOLDER}/${PROJECT_NAME}.md"
+
+cd "$TMP_FOLDER"
+git add "${DOC_FOLDER}/${PROJECT_NAME}.md"
+git config user.name "CI/CD"
+git config user.email "info@byjg.com.br"
+git commit -m "Update documentation '$DOC_FOLDER' '$PROJECT_NAME'"
+git push
