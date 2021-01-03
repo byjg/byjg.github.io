eff350f3bb14716ede9da8941f3da28ed5e299f3
Joao Gilberto Magalhaes <joao@byjg.com.br>
2021-01-03 14:51:47 -0600

    Add feature extra documentation to the add-doc.sh

--- add-doc.sh
+++ add-doc.sh
@@ -22,10 +22,17 @@ TMP_FOLDER=/tmp/byjg.github.io
 
 git clone --depth 1 https://byjg:$DOC_GITHUB_TOKEN@github.com/byjg/byjg.github.io $TMP_FOLDER
 
-cp README.md "${TMP_FOLDER}/${DOC_FOLDER}/${PROJECT_NAME}.md"
+if [ -z "$EXTRA_FOLDER" ]
+then
+  cp README.md "${TMP_FOLDER}/${DOC_FOLDER}/${PROJECT_NAME}.md"
+else
+  mkdir "${TMP_FOLDER}/${DOC_FOLDER}/${PROJECT_NAME}"
+  cp README.md "${TMP_FOLDER}/${DOC_FOLDER}/${PROJECT_NAME}"
+  cp -r "$EXTRA_FOLDER/*" "${TMP_FOLDER}/${DOC_FOLDER}/${PROJECT_NAME}"
+fi
 
 cd "$TMP_FOLDER"
-git add "${DOC_FOLDER}/${PROJECT_NAME}.md"
+git add "${DOC_FOLDER}/${PROJECT_NAME}*"
 git config user.name "CI/CD"
 git config user.email "info@byjg.com.br"
 git commit -m "Update documentation '$DOC_FOLDER' '$PROJECT_NAME'"
