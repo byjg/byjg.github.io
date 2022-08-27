bbaed5a59a043010f7070acc5e74c8a82ffd52c5
Joao M <joao@byjg.com.br>
2022-08-27 20:26:30 +0000

    Add helm repository

---
+++ add-helm.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+HELM_FOLDER="$1"
+HELM_PROJECT="$2"
+
+if [ -z "$HELM_PROJECT" ]
+then
+  echo "Usage: "
+  echo "  add-helm.sh <helm_folder> <helm_project>"
+  exit 1
+fi
+
+if [ -z "$DOC_GITHUB_TOKEN" ]
+then
+  echo 'DOC_GITHUB_TOKEN is required'
+  exit 2
+fi
+
+PROJECT_FOLDER="$PWD"
+TMP_FOLDER=/tmp/byjg.github.io
+
+git clone --depth 1 https://byjg:$DOC_GITHUB_TOKEN@github.com/byjg/byjg.github.io $TMP_FOLDER
+
+# Install helm
+wget https://get.helm.sh/helm-v3.9.4-linux-amd64.tar.gz -o /dev/null -O helm.tar.gz
+tar xvf helm.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin
+helm version
+
+# Build package
+cd $HELM_FOLDER
+helm package $HELM_PROJECT
+mv $HELM_PROJECT*.tgz $TMP_FOLDER/helm
+helm repo index $TMP_FOLDER/helm --url https://opensource.byjg.com.br/helm
+
+cd "$TMP_FOLDER"
+git add "${DOC_FOLDER}/helm/*"
+git config user.name "CI/CD"
+git config user.email "info@byjg.com.br"
+git commit -m "Add helm package '$HELM_PROJECT'"
+git push
---
+++ helm/README.md
@@ -0,0 +1,15 @@
+---
+sort: 4
+---
+
+# Helm Repository
+
+Here you can find the Helm Repository for our opensource projects
+
+To see the packages and get more information use the command bellow:
+
+```bash
+helm repo add byjg https://opensource.byjg.com/helm
+helm repo list
+helm search repo byjg
+```
